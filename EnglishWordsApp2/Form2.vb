af0317fbfacd1e51593b9768de7f404146e573ed
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-11-30 23:49:50 +0900

    ソースコード整理

--- EnglishWordsApp2/Form2.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form2
-    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button.Click
-
-    End Sub
-End Class
