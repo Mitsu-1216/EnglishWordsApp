249585f87b9e0d4bb4eab1888e19f682779384de
Mitsu-1216 <adieuer._.03.26@icloud.com>
2024-01-13 19:01:04 +0900

    iをcountに修正・Number削除

--- EnglishWordsApp2/Form1.vb
+++ EnglishWordsApp2/Form1.vb
@@ -1,23 +1,22 @@
 ﻿Public Class Form1
-    Dim Number As Integer = 1
     Dim EnglishProblemWords() As String = {"リンゴ", "学校", "鉛筆", "机", "鞄", "本", "部屋", "コップ", "椅子", "頭"}
     Dim EnglishAnswerWords() As String = {"apple", "school", "pencil", "desk", "bag", "book", "room", "cup", "chair", "head"}
-    Dim i As Integer = 0
+    Dim count As Integer = 0
 
     Private Sub problemNumber_Click(sender As Object, e As EventArgs) Handles MyBase.Load
-        problemNumber.Text = "第" + (i + 1).ToString + "門"
+        problemNumber.Text = "第" + (count + 1).ToString + "門"
     End Sub
 
     Private Sub NextButton_Click(sender As Object, e As EventArgs) Handles NextButton.Click
-        i = i + 1
-        EnglishProblem.Text = EnglishProblemWords(i)
-        EnglishAnswer.Text = EnglishAnswerWords(i)
+        count = count + 1
+        EnglishProblem.Text = EnglishProblemWords(count)
+        EnglishAnswer.Text = EnglishAnswerWords(count)
         EnglishAnswer.Visible = False
-        problemNumber.Text = "第" + (i + 1).ToString + "門"
-        If i = EnglishProblemWords.Count - 1 Then
+        problemNumber.Text = "第" + (count + 1).ToString + "門"
+        If count = EnglishProblemWords.Count - 1 Then
             NextButton.Enabled = False
         End If
-        If Not i = 0 Then
+        If Not count = 0 Then
             BackButton.Enabled = True
         End If
         correctAnswer.Text = ""
@@ -26,25 +25,25 @@
     End Sub
 
     Private Sub BackButton_Click(sender As Object, e As EventArgs) Handles BackButton.Click
-        If i > 0 Then
-            i = i - 1
-            EnglishProblem.Text = EnglishProblemWords(i)
+        If count > 0 Then
+            count = count - 1
+            EnglishProblem.Text = EnglishProblemWords(count)
         End If
-        problemNumber.Text = "第" + (i - 1).ToString + "門"
-        If i = 0 Then
+        problemNumber.Text = "第" + (count - 1).ToString + "門"
+        If count = 0 Then
             BackButton.Enabled = False
         End If
-        If Not i = EnglishProblemWords.Count - 1 Then
+        If Not count = EnglishProblemWords.Count - 1 Then
             NextButton.Enabled = True
         End If
         correctAnswer.Text = ""
         TextBoxAnswer.Text = ""
-        EnglishAnswer.Text = EnglishAnswerWords(i)
+        EnglishAnswer.Text = EnglishAnswerWords(count)
         EnglishAnswer.Visible = False
     End Sub
 
     Private Sub BackButton_property(sender As Object, e As EventArgs) Handles MyBase.Load
-        If i = 0 Then
+        If count = 0 Then
             BackButton.Enabled = False
         End If
     End Sub
@@ -52,7 +51,7 @@
     '次の問題
     Private Sub answer_Click(sender As Object, e As EventArgs) Handles answer.Click
         EnglishAnswer.Visible = True
-        If TextBoxAnswer.Text = EnglishAnswerWords(i) Then
+        If TextBoxAnswer.Text = EnglishAnswerWords(count) Then
             correctAnswer.Text = "〇"
             correctAnswer.ForeColor = Color.Blue
         Else
